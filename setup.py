96f2f06fe1180dc79545b328a828b938d6d872db
Sai Ruthvik <msairuthvik@gmail.com>
2025-07-27 14:32:32 -0500

    added the requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chabot",
+    version="0.1.0",
+    author="Sai Ruthvik Munugoti",
+    author_email="msairuthvik@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
